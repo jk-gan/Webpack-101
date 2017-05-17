c84d7947e0ae8e0dbfb03113d12f56846f161f51
Gan Jun Kai <kuhn96@gmail.com>
2017-05-17 21:21:00 +0800

    Add loader to load images and fonts

--- project/src/app.js
+++ project/src/app.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import logo from './logo.svg'
 import css from './app.scss'
 
+const App = () => {
+  return(
+    <div>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>Hello, world!</h1>
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <App />,
   document.getElementById('root')
 )
--- project/webpack.config.js
+++ project/webpack.config.js
@@ -27,6 +27,28 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: 'babel-loader'
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|svg)$/i,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            name: '[name].[ext]',
+            outputPath: './images/'
+          }
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            name: '[name].[ext]',
+            outputPath: './fonts/'
+          }
+        }
       }
     ]
   },
