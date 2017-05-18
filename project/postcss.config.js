54fdb30c9719905902d1cb54dc189a79c99d7d6e
Gan Jun Kai <kuhn96@gmail.com>
2017-05-18 21:12:56 +0800

    Add diff browser support for autoprefixer

--- project/postcss.config.js
+++ project/postcss.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   plugins: {
-    'autoprefixer': {},
+    autoprefixer: {
+      browsers: [
+        '>1%',
+        'last 4 versions',
+        'Firefox ESR',
+        'not ie < 9', // React doesn't support IE8 anyway
+      ]
+    },
   }
 }
