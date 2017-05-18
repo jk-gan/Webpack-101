0d14935e41476f47cd15af996d01e770b99c19dc
Gan Jun Kai <kuhn96@gmail.com>
2017-05-18 14:19:46 +0800

    Make the source map production ready

--- project/webpack.config.js
+++ project/webpack.config.js
@@ -85,5 +85,5 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin(), // enable HMR globally
     new webpack.NamedModulesPlugin(), // prints more readable module names in the browser console on HMR updates
   ],
-  devtool: "eval-source-map"
+  devtool: isProduction ? "source-map" : "eval-source-map"
 }
